d82611c40cfebb7804aa55d590d1c946b1939e4e
Margot.L.L <mleletty@hotmail.com>
2020-09-24 09:48:54 +0200

    Test video youtube

--- src/pages/margotpage.js
+++ src/pages/margotpage.js
@@ -55,6 +55,12 @@ function MargotPage(props) {
           )}
         </tbody>
       </Table>
+      <h2>Et en bonus !</h2>
+      <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/4pkhEyRoidk" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen  />
+      </div>
     </Navigation>
   )
 }
