e21ebbfbb9f523360611d33deeac35372585b69d
Jules VANDEWIELE <j.vandewiele@attineos.com>
2020-09-24 15:35:30 +0200

    solve issue

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,19 +4,15 @@ import styled from 'styled-components'
 import {CustomLink} from '../Navigation/styles/CustomLink'
 import {CustomSection} from '../Navigation/styles/CustomSection'
 
-const Navigation =  ({children}) => {
-
-  return(
-    <div>
-      <CustomSection>
-        <CustomLink to="/">Home</CustomLink>
-        <CustomLink to="/blog/">Blog</CustomLink>
-        <CustomLink to="/julespage/">JulesPage</CustomLink>
-        <CustomLink to="/margotpage/">MargotPage</CustomLink>
-      </CustomSection>
-      {children}
-    </div>
-  )
-}
-
+const Navigation =  ({children}) => 
+  <div>
+    <CustomSection>
+      <CustomLink to="/">Home</CustomLink>
+      <CustomLink to="/blog/">Blog</CustomLink>
+      <CustomLink to="/julespage/">JulesPage</CustomLink>
+      <CustomLink to="/margotpage/">MargotPage</CustomLink>
+    </CustomSection>
+    {children}
+  </div>
+  
 export default Navigation;
