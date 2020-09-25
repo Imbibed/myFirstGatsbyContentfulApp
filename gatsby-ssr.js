e9d3075066e46b3230c001c5d469d3ad75fae896
Jules VANDEWIELE <j.vandewiele@attineos.com>
2020-09-25 14:32:09 +0200

    setup all gatsby files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navigation from "./src/components/Navigation";
-import { ThemeProvider } from "styled-components";
 import theme from './src/theme'
 import { mailjet, ThemePartialProvider } from "mailjet-react-components";
 
