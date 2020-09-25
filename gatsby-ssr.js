da07896b1c5f2b2c5a69d054cb4983141076856e
Antoine Chan <antoinectsmad@gmail.com>
2020-09-25 14:10:27 +0200

    add minimal theme and Layout in g-browser + g-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Navigation from "./src/components/Navigation";
+import { ThemeProvider } from "styled-components";
+import theme from './src/theme'
 
-export const wrapPageElement = ({children}) => <Navigation>
-  {children}
-</Navigation>
+export const wrapPageElement = ({element}) => <ThemeProvider theme={theme}>
+  <Navigation>
+    {element}
+  </Navigation>
+</ThemeProvider>
