3001154883f42ae68d21db6ee6e7d162a4d7e4a8
Margot.L.L <mleletty@hotmail.com>
2020-09-28 11:49:49 +0200

    Hook personnalisé

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import Navigation from '../components/Navigation'
-import styled from 'styled-components'
 
 function RootIndex(props){
   return (
-    <div>
-      <Navigation>
-        <p>Index</p>
-      </Navigation>
-    </div>
+    <p>Index</p>
   )
 }
 
