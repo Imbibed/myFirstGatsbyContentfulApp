3aeec690ca16c4ec9d2d446ba04cbce046479f85
Jules Vandewiele <jvandewiele@mailgun.com>
2020-09-30 09:26:05 +0200

    import Navigation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import theme from './src/theme'
 import { mailjet, ThemePartialProvider } from "mailjet-react-components";
+import Navigation from './src/components/Navigation'
 
 export const wrapPageElement = ({ element }) => <ThemePartialProvider theme={mailjet} otherTheme={theme} loadFonts={false}>
   <Navigation>
