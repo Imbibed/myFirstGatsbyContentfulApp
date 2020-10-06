121b2b1558feb250a5700f40f41c858d973849c3
Jules Vandewiele <jvandewiele@mailgun.com>
2020-10-06 17:11:27 +0200

    standby

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,10 +38,12 @@ exports.createPages = async ({graphql, actions, reporter}) => {
             nationality
             isCold
             picture {
-              file {
-                url
-                fileName
+              image {
+                file {
+                  url
+                }
               }
+              alt
             }
           }
         }
