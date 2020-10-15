b4f8233cca83ce4c45775ec0b2dabef57d5eb9bc
Jules Vandewiele <jvandewiele@mailgun.com>
2020-10-15 11:33:33 +0200

    add utils files

--- src/Utils/TranslationService.js
+++ src/Utils/TranslationService.js
@@ -1,8 +1,9 @@
-const TranslationTools = {
-  table: {
-    en: {name: 'English', contentfulName: 'en-US'},
-    fr: {name: 'Français', contentfulName: 'fr'}
-  }
+import {styledFrenchFlag} from './FrenchFlag'
+import {styledEnglishFlag} from './EnglishFlag'
+
+export const languageTable = {
+  en: {name: 'English', contentfulName: 'en-US', icon: styledEnglishFlag},
+  fr: {name: 'Français', contentfulName: 'fr', icon: styledFrenchFlag}
 }
 
-export default TranslationTools;
+export const languages = Object.keys(languageTable);
--- src/components/MailjetNavigation/styles/NavContainer.js
+++ src/components/MailjetNavigation/styles/NavContainer.js
@@ -2,8 +2,6 @@ import styled from 'styled-components'
 
 export const NavContainer = styled.div`
   max-width: 1280px;
-  margin-left:auto;
-  margin-right:auto;
   padding-left:${({theme}) => {theme.sizes.margins.m5}};
   padding-right:${({theme}) => {theme.sizes.margins.m5}};
   display:flex;
