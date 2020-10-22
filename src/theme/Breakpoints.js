987b2d252e934ff2460e131aea1470f44a2b08f1
Antoine Chan <achantchisong@mailgun.com>
2020-10-22 17:04:33 +0200

    Update react compo + add maxWidth theme props for container

--- src/theme/Breakpoints.js
+++ src/theme/Breakpoints.js
@@ -1,18 +1,23 @@
 const Breakpoints = {
   mobile: {
     breakpoint: 0,
-    containerMaxWidth: "780px",
-    containerPadding: "0",
+    maxBreakpoint: 767,
+    containerMaxWidth: "690px",
+    containerPadding: "0 s4",
+    width: 'auto',
   },
   tablet: {
-    breakpoint: 780,
-    containerMaxWidth: "1100px",
-    containerPadding: "0",
+    breakpoint: 768,
+    maxBreakpoint: 1024,
+    containerMaxWidth: "920px",
+    containerPadding: "0 s5",
+    width: 'auto',
   },
   desktop: {
-    breakpoint: 1280,
-    containerMaxWidth: "1460px",
-    containerPadding: "0",
+    breakpoint: 1025,
+    containerMaxWidth: "970px",
+    containerPadding: "0 s5",
+    width: 'auto',
   }
 }
 
